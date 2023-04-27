c610c6110fb97c2d5ead4f9e1cddeaac867ee2bb
Natto <zx4545zx.zx4545zx@gmail.com>
2023-04-27 13:55:46 +0700

    add food page

--- src/routes/Food.svelte
+++ src/routes/Food.svelte
@@ -1,10 +1,22 @@
 <script>
   import Slide from "../lib/Slide.svelte";
   import Banner from "../lib/Banner.svelte";
+  import Card from "../lib/Card.svelte";
+
+  import { slideImages } from "../data/foodSlide";
+  import { landingImages } from "../data/foodLanding";
 </script>
 
-<Slide />
+<Slide images={slideImages}/>
 <Banner title="Food" />
+
+<main class="h-3/5 sm:h-screen pt-4">
+  <div class="container mx-auto overflow-y-scroll sm:overflow-y-visible h-full">
+    {#each landingImages as img}
+      <Card url={img} />
+    {/each}
+  </div>
+</main>
 
 <main>
 </main>
